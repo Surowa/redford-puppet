7ce17c0132dbab9ecabc1b2a223e1f10be82f470
Surowa <robertwagensveld2@live.nl>
2019-09-28 16:34:20 +0200

    Update init.pp

--- modules/raspberry/manifests/init.pp
+++ modules/raspberry/manifests/init.pp
@@ -1,5 +1,23 @@
 class raspberry {
 
+include apt
+
+apt::source { 'pilight.list':
+  comment  => 'pilight',
+  location => 'http://apt.pilight.org/',
+  release  => 'stable',
+  repos    => 'main',
+  },
+  include  => {
+    'src' => true,
+    'deb' => true,
+  },
+}
+
+apt::key { 'pilight':
+  server  => 'http://apt.pilight.org/pilight.key',
+}
+
 package { 'python3': ensure => installed, }
 package { 'git': ensure => installed, }
 package { 'pilight': ensure => installed, }
