4e0f9ba36a12bf5e38a18e54b74f203b3fa7e902
Surowa <robertwagensveld2@live.nl>
2019-10-06 17:35:50 +0200

    Update init.pp

--- modules/raspberry/manifests/init.pp
+++ modules/raspberry/manifests/init.pp
@@ -1,7 +1,6 @@
 class raspberry {
 
 include apt
-include apt::update
 
 exec { 'pilight':
   command  => 'echo "deb http://apt.pilight.org/ stable main" > /etc/apt/sources.list.d/pilight.list && wget -O - http://apt.pilight.org/pilight.key | apt-key add -',
