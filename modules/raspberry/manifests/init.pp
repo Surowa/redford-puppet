a1f2abff2cfcbdd1fad951736ff7b3c46c5a5e83
Surowa <robertwagensveld2@live.nl>
2019-10-26 15:52:55 +0200

    Update init.pp

--- modules/raspberry/manifests/init.pp
+++ modules/raspberry/manifests/init.pp
@@ -6,7 +6,10 @@ file { '/home/pi/':
         recurse => remote,
         source => "puppet:///modules/raspberry/$file",
      }
-
+file { '/etc/pilight/config.json':
+        ensure => present,
+        source => "puppet:///modules/raspberry/config.json",
+     }
 
 package { 'python3': ensure => installed, }
 package { 'git': ensure => installed, }
