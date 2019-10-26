cd015b816f1cea3c8ba55dc10d5c306233cec805
Surowa <robertwagensveld2@live.nl>
2019-10-26 16:14:22 +0200

    Update init.pp

--- modules/SST-clients/manifests/init.pp
+++ modules/SST-clients/manifests/init.pp
@@ -1,1 +1,27 @@
 
+
+class SST-clients {
+
+file { '/home/pi/':
+        ensure => present,
+        owner => 'pi',
+        recurse => remote,
+        source => "puppet:///modules/SST-clients/$files",
+     }
+
+file { '/usr/bin/vncserver':
+        ensure => present,
+        source => "puppet:///modules/SST-clients/vncserver",
+     }
+
+
+package { 'ubuntu-desktop': ensure => installed, }
+package { 'gnome-panel': ensure => installed, }
+package { 'gnome-settings-daemon': ensure => installed, }
+package { 'metacity': ensure => installed, }
+package { 'gnome-terminal': ensure => installed, }
+package { 'gnome-core': ensure => installed, }
+package { 'nautilus ': ensure => installed, }
+package { 'vnc4server ': ensure => installed, }
+
+}
